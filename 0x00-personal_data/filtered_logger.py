bd15d7d6270df7b6423efca4fe777107fa286cf2
5237-mests <msfnmulgeta@gmail.com>
2023-02-02 11:49:17 +0300

    first

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module
+"""
+import logging
+import re
+from typing import List
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """Func"""
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
