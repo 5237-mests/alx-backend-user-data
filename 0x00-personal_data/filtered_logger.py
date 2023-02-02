a356d939687ded346e3a018590b2dff1d8fc32e5
5237-mests <msfnmulgeta@gmail.com>
2023-02-02 17:02:31 +0300

    getlogger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,12 +11,27 @@ patterns = {
 }
 
 
+PII_FIELDS = ("name", "email", "phone", "ip", "last_login")
+
+
 def filter_datum(
       fields: List[str], redaction: str, message: str, separator: str
       ) -> str:
     """Func return info"""
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+def get_logger() -> logging.Logger:
+    """Creates a new logger for user data.
+    """
+    logger = logging.getLogger("user_data")
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
