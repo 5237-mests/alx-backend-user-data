d39186cb196dc4ae26802a9ff1be898ca101957a
5237-mests <msfnmulgeta@gmail.com>
2023-02-08 11:43:27 +0300

    auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 """basic auth module
 """
+import re
+
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """Class BasicAuth
     """
-    pass
+    def extract_base64_authorization_header(
+                                            self,
+                                            authorization_header: str
+                                            ) -> str:
+        """extract base64
+
+        Args:
+            authorization_header (str): header
+
+        Returns:
+            str: Base64 part of the Authorization header
+        """
+        if authorization_header is None or \
+                type(authorization_header) != str or \
+                re.search('Basic ', authorization_header) is None:
+            return None
+        return authorization_header[6:]
