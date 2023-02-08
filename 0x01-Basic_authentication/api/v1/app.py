f5cf40e58694ab3bb7cdbe0d3181a2952a42c152
5237-mests <msfnmulgeta@gmail.com>
2023-02-08 10:46:28 +0300

    authtype

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,9 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = getenv('AUTH_TYPE', None)
+auth = None
 
-if auth:
+AUTH_TYPE = getenv('AUTH_TYPE')
+
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
