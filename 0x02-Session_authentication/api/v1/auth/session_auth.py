db7d9e05cf563eb0168a1c14c0b5b6e26f289f15
5237-mests <msfnmulgeta@gmail.com>
2023-02-09 22:01:24 +0300

    sesId

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,25 @@
 """
 from api.v1.auth.auth import Auth
 
+import uuid
+
 
 class SessionAuth(Auth):
     """Session class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id:
+
+        Args:
+            user_id (str, optional): user id. Defaults to None.
+
+        Returns:
+            str: Session ID
+        """
+        if type(user_id) == str:
+            ID = str(uuid.uuid4())
+            self.user_id_by_session_id[ID] = user_id
+            return ID
+        return None
