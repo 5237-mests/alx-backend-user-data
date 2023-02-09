d55b13f6b8e505eb832fff56230007bd189d6530
5237-mests <msfnmulgeta@gmail.com>
2023-02-09 23:10:42 +0300

    app

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,11 +34,13 @@ def before_request():
     listofpath = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth:
         if auth.require_auth(request.path, listofpath):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
