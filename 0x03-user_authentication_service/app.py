4a11871c257c2c237d795c35409b53e7d3686700
5237-mests <msfnmulgeta@gmail.com>
2023-02-15 22:12:44 +0300

    users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from sqlalchemy.orm.exc import NoResultFound
+
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -11,6 +16,19 @@ app = Flask(__name__)
 def index():
     """index"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """route user"""
+    password = request.form.get("password")
+    email = request.form.get("email")
+    try:
+        AUTH._db.find_user_by(email=email)
+    except NoResultFound:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
