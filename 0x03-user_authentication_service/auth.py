7457af8a8c6a02b9d23bcc2d64b458f5f94bd028
5237-mests <msfnmulgeta@gmail.com>
2023-02-15 22:55:53 +0300

    uuid4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,7 +15,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """generate uuid str"""
     return str(uuid.uuid4())
 
