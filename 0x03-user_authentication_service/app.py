4b695e27dba4e5b199467b02ac0cee2c6967edc0
5237-mests <msfnmulgeta@gmail.com>
2023-02-16 14:43:58 +0300

    routepssw

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -78,5 +78,19 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route("/reset_password", methods=['PUT'], strict_slashes=False)
+def update_password():
+    """update PASSWORD"""
+    email = request.form.get('email')
+    reset_token = request.cookies.get("reset_token")
+    new_password = request.form.get('new_password')
+    try:
+        user = AUTH._db.find_user_by(reset_token=reset_token)
+    except NoResultFound:
+        abort(403)
+    AUTH.update_password(reset_token, new_password)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
